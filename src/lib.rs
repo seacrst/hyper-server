9737eac1cbaf99eabf5c46856b29b09afb595fcb
Seacrest <paganseacrest@gmail.com>
2025-01-08 12:43:31 +0200

    requst handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,94 @@
+use std::{convert::Infallible, sync::Arc};
 
-use std::future::Future;
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::{
+  body::Incoming, HeaderMap, Method, Request, Response
+};
+use reqwest::Client;
+use tokio::sync::RwLock;
 
-use hyper::rt::Executor;
-
-#[derive(Default, Debug, Clone)]
-pub struct TokioExecutor;
-
-impl<F> Executor<F> for TokioExecutor
-where F: Future + Send + 'static, F::Output: Send + 'static {
-    fn execute(&self, fut: F) {
-        tokio::spawn(fut);
-    }
+const THRESHOLD: u64 = 5000;
+pub struct Worker {
+  pub addr: String,
+  pub cons_num: u64
 }
 
-impl TokioExecutor {
-  pub fn new() -> Self {
-    Self {}
+impl Worker {
+  pub fn new(addr: String) -> Self {
+    Self {
+      addr,
+      cons_num: 0
+    }
   }
+
+  pub fn increase(&mut self) {
+    self.cons_num += 1;
+  }
+
+  pub fn decrease(&mut self) {
+    if self.cons_num > 0 {
+      self.cons_num -= 1;
+    }
+  }
+
+  pub fn is_busy(&self) -> bool {
+    self.cons_num > THRESHOLD
+  }
+}
+
+pub type RwProxy = Arc<RwLock<Proxy>>;
+
+pub struct Proxy {
+  workers: Vec<Worker>,
+  pub current: usize
+}
+
+impl Proxy {
+  pub fn new(workers: Vec<Worker>) -> Self {
+    Self {
+      workers,
+      current: 0
+    }
+  }
+
+  pub async fn handle_req(&mut self, req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    let index_worker_tup = self.workers.iter()
+      .enumerate()
+      .find(|(i, w)| {
+        match self.workers.get(i + 1) {
+          Some(next_w) => {
+            if w.is_busy() && next_w.is_busy() {
+              w.cons_num < next_w.cons_num
+            } else {
+              !w.is_busy()
+            }
+          },
+          None => !w.is_busy()
+        }
+      });
+    
+    match index_worker_tup {
+        Some((index, _)) => {
+          self.current = index;
+          let worker = self.workers.get_mut(index).unwrap();
+          execute(worker.addr.clone(), req);
+        },
+        None => {
+          hhdh
+        }
+    }
+    
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+  }
+}
+
+
+async fn execute(host: String, req: Request<Incoming>) -> Result<reqwest::Response, reqwest::Error> {
+  let client = Client::new();
+  client.request(req.method(), url)
+    .headers(req.headers())
+    .body(req.body())
+    .send()
+    .await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
-use std::{convert::Infallible, net::SocketAddr};
+use std::{convert::Infallible, net::SocketAddr, sync::Arc};
 
 use bytes::Bytes;
 use http_body_util::Full;
-use hyper::{server::conn::http1::Builder, service::service_fn, Request, Response};
-use tokio::net::TcpListener;
-use hyper_util::rt::{TokioExecutor, TokioIo};
+use hyper::{body::Incoming, service::service_fn, Request, Response};
+use hyper_server::{Proxy, RwProxy, Worker};
+use tokio::{net::TcpListener, sync::RwLock};
+use hyper_util::rt::TokioIo;
+use hyper::server::conn::http1;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
   let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
   let listener  = TcpListener::bind(addr).await?;
+
+  let workers = vec![
+    Worker::new(String::from("http://localhost:3001")),
+    Worker::new(String::from("http://localhost:3002")),
+    Worker::new(String::from("http://localhost:3003"))
+  ];
+  
+  let proxy = Arc::new(RwLock::new(Proxy::new(workers)));
   
   loop {
-    let (stream, _) = listener .accept().await?;
+    let (stream, _) = listener.accept().await?;
     let io = TokioIo::new(stream);
+    let proxy = proxy.clone();
     
     tokio::task::spawn(async move {
-      if let Err(err) = hyper::server::conn::http2::Builder::new(TokioExecutor::new())
-        .serve_connection(io, service_fn(hello))
+      if let Err(err) = http1::Builder::new()
+        .serve_connection(io, service_fn(move |req| handle_req(req, proxy.clone())))
         .await {
             eprintln!("Error serving connection: {}", err);
         }
     });
   }
-  
 }
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-  Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
-}
-async fn handle_req(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
 
-  let json_data = serde_json::to_string("").unwrap();
-
-  let res = Response::new(Full::new(Bytes::from(json_data)));
-
-  Ok(res)
+async fn handle_req<'a>(req: Request<Incoming>, proxy: RwProxy) -> Result<Response<Full<Bytes>>, Infallible> {
+  proxy.write()
+    .await
+    .handle_req(req)
 }
